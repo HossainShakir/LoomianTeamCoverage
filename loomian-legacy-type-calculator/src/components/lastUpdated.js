0bfc7c2f8f7dd1280feb0ac62c074ea7ee5b9c56
HossainShakir <87627777+HossainShakir@users.noreply.github.com>
2025-04-19 21:47:29 -0400

    easter items added

--- loomian-legacy-type-calculator/src/components/lastUpdated.js
+++ loomian-legacy-type-calculator/src/components/lastUpdated.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../App.css'; 
 
 const changelog = `
-- Added Soulburst Dreggodyne
+- Easter items added
 `;
 
 const LastUpdated = () => {
@@ -19,7 +19,7 @@ const LastUpdated = () => {
     return (
         <div>
             <div id="last-updated-overlay" onClick={handleOverlayClick}>
-                Last Updated: 2025-04-13
+                Last Updated: 2025-04-19
             </div>
             {showChangelog && (
                 <div className="changelog-overlay">
